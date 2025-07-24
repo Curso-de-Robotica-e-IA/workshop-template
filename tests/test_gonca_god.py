3ce3798bf0a2103921d6481b54c25fcaedcb6ec7
goncamateus <mgm4@cin.ufpe.br>
2025-07-23 21:51:18 -0300

    FEAT: Add sample function and corresponding test

---
+++ tests/test_gonca_god.py
@@ -0,0 +1,6 @@
+from workshop.gonca_god import gonca_god
+
+
+def test_gonca_god():
+    """Test the gonca_god function."""
+    assert gonca_god() == "Gonca God"
---
+++ workshop/gonca_god.py
@@ -0,0 +1,3 @@
+def gonca_god():
+    """Function to return a string."""
+    return "Gonca God"
